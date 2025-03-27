386efd5ee11846b978cd2ffa24f7a609c79b23c3
jay snow <jc123593@gmail.com>
2025-03-27 19:51:25 +0800

    "changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">clear
         <img src={logo} className="App-logo" alt="logo" />
-       deployment!
+       deployment
       </header>
     </div>
   );
